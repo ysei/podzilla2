897fba611c5a71335efb822b455121d132e5dc2f
Jeremie <jerl1992@gmail.com>
2013-07-23 11:18:46 -0400

    igpsp - update and fix most of all

--- modules/igpSP/igpSP.c
+++ modules/igpSP/igpSP.c
@@ -1,8 +1,8 @@
 /*
- * Last updated: May 18, 2008
- * ~Keripo
+ * Last updated: Jul 16, 2013
+ * ~Jerl92
  *
- * Copyright (C) 2008 Keripo
+ * Copyright (C) 2008-2013 Keripo Jerl92
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,106 +19,149 @@
  * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
+#include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include "pz.h"
 
-static PzConfig *igpsp_conf;
-static PzModule *igpsp_module;
-static ttk_menu_item igpsp_browser_extension;
-static const char *igpsp_binary, *igpsp_dir;
+static PzConfig *config;
+static PzModule *module;
+static ttk_menu_item browser_extension;
+static const char *binary;
 
-extern TWindow *open_directory_ext(const char *filename, const char *header, int ext(const char *file));
-static TWidget *(*ltinstw)(int x, int y, int w, int h, int absheight,
-		char *dt, int (*callback)(TWidget *, char *));
-static int (*ltws)(TWidget * wid);
+static TWindow *(*open_directory_ext_softdep)(const char *filename, const char *title, int ext(const char *file));
 
-int text_available()
+static int (*ti_widget_start_softdep)(TWidget * wid);
+static TWidget *(*ti_new_standard_text_widget_softdep)(int x, int y, int w, int h, int absheight, char *dt, int (*callback)(TWidget *, char *));
+static void (*ti_multiline_text_softdep)(ttk_surface srf, ttk_font fnt, int x, int y, int w, int h, ttk_color col, const char *t, int cursorpos, int scroll, int * lc, int * sl, int * cb);
+
+static int browser_available()
 {
-	if (!ltws)
-		ltws = pz_module_softdep("tiwidgets", "ti_widget_start");
-	if (!ltinstw)
-		ltinstw = pz_module_softdep("tiwidgets",
-				"ti_new_standard_text_widget");
-	return (!!ltinstw & !!ltws);
+	if (!open_directory_ext_softdep)
+	  open_directory_ext_softdep = pz_module_softdep("browser", "open_directory_ext");
+        return (!!open_directory_ext_softdep);
 }
 
-int is_gba(const char *file)
+static int tiwidgets_available()
+{
+	if (!ti_widget_start_softdep)
+	  ti_widget_start_softdep = pz_module_softdep("tiwidgets", "ti_widget_start");
+	if (!ti_new_standard_text_widget_softdep)
+	  ti_new_standard_text_widget_softdep = pz_module_softdep("tiwidgets", "ti_new_standard_text_widget");
+        if (!ti_multiline_text_softdep)
+	  ti_multiline_text_softdep = pz_module_softdep("tiwidgets", "ti_multiline_text");
+	return (!!ti_widget_start_softdep & !!ti_new_standard_text_widget_softdep & !!ti_multiline_text_softdep);
+}
+
+static int is_gba(const char *file)
 {
         return check_ext(file, ".gba");
 }
 
-static PzWindow *igpsp_load_file(const char *file)
-{
-        char * command_igpsp = (char *) calloc(sizeof(command_igpsp), sizeof(char));
-	strcpy(command_igpsp, "igpSP ");
-	strcat(command_igpsp, file);
-	pz_exec(pz_module_get_datapath (igpsp_module, command_igpsp));
+static PzWindow *exec_file(const char *file)
+{ 
+        char * command = (char *) calloc(120, sizeof(char));
+	strcpy(command, "igpSP ");
+	strcat(command, file);
+	pz_exec(pz_module_get_datapath (module, command));
 	return TTK_MENU_DONOTHING;
 }
 
-static PzWindow *load_igpsp_handler(ttk_menu_item *item)
+static PzWindow *load_handler(ttk_menu_item *item)
 {
-	return igpsp_load_file(item->data);
+	return exec_file(item->data);
 }
 
-static PzWindow *browse_igpsp_roms()
+static PzWindow *browse_roms()
 {
-	igpsp_dir = pz_get_string_setting(igpsp_conf, 1);
-        return open_directory_ext(igpsp_dir, "Game Boy Advance", is_gba);
-}
+        struct stat st;
+        char * folder = (char *) pz_get_string_setting(config, 1);
 
-static int set_igpsp_directory(TWidget * wid, char * txt)
-{
-        pz_set_string_setting(igpsp_conf, 1, txt);
-        pz_save_config(igpsp_conf);
-        pz_close_window(wid->win);
-        return 0;
-}
-
-static PzWindow * igpsp_directory()
-{
-        if (!text_available()) {
-           pz_error(_("Tiwidgets(Text input) is missing."));
-            mkdir("/mnt/rom//", S_IFDIR|S_IRUSR|S_IWUSR|S_IXUSR|S_IXGRP|S_IWGRP|S_IRGRP);
-             set_igpsp_directory(NULL, "/mnt/rom/igpSP/");
-            pz_error(_("Until you install tiwidgets your Rom folder will be /mnt/rom/igpSP/"));
-          return TTK_MENU_UPONE;
-         } else {
-	  PzWindow * ret;
-	   TWidget * wid;
-	    ret = pz_new_window(_("igpSP Rom Directory"), PZ_WINDOW_NORMAL);
-             wid = ltinstw(10, 40, ret->w-20, 10+ttk_text_height(ttk_textfont), 0, "/mnt/", set_igpsp_directory);
-	    ttk_add_widget(ret, wid);
- 	   ltws(wid);
-          return pz_finish_window(ret);
+        if (stat (folder, &st) < 0 || !S_ISDIR (st.st_mode)) {
+          pz_warning("Folder do not exsist or have been not selected");
+          pz_warning("Select a valid Folder in Setting -> Select folder -> Game Boy Advance");  
+          return TTK_MENU_DONOTHING;
         }
+        
+        if (browser_available()) {
+          return open_directory_ext_softdep(folder, "Game Boy Advance", is_gba);
+        } 
+
+        pz_warning("Browser module is missing.");
+        return TTK_MENU_DONOTHING;
 }
 
-static void igpsp_cleanup()
+static void select_folder_draw(TWidget * wid, ttk_surface srf)
+{
+	ttk_ap_fillrect(srf, ttk_ap_getx("window.bg"), wid->x, wid->y, wid->w, wid->h);
+	if (ttk_screen->w < 160) {
+		ti_multiline_text_softdep(srf, ttk_textfont, wid->x, wid->y, wid->w, wid->h, ttk_ap_getx("window.fg")->color,
+			_("Type the ROM folder location."), -1, 0, 0, 0, 0);
+	} else if (ttk_screen->w < 200) {
+		ti_multiline_text_softdep(srf, ttk_textfont, wid->x, wid->y, wid->w, wid->h, ttk_ap_getx("window.fg")->color,
+			_("Type the folder where the ROM are located."), -1, 0, 0, 0, 0);
+	} else {
+		ti_multiline_text_softdep(srf, ttk_textfont, wid->x, wid->y, wid->w, wid->h, ttk_ap_getx("window.fg")->color,
+			_("Type the folder where the ROM are located. To be able tu use ROM selector in the Extras menu."), -1, 0, 0, 0, 0);
+	}
+}
+
+static int save_folder(TWidget * wid, char * fn)
+{	
+        pz_set_string_setting(config, 1, fn);
+        pz_save_config(config);
+        pz_ipod_fix_settings(config);
+        pz_close_window(wid->win);
+	return 0;
+}
+
+static PzWindow * select_folder()
+{
+        if (tiwidgets_available()) {
+	PzWindow * ret;
+	TWidget * wid;
+	TWidget * wid2;
+	ret = pz_new_window(_("Game Boy Advance"), PZ_WINDOW_NORMAL);
+	wid = ti_new_standard_text_widget_softdep(10, 
+                       10+ttk_text_height(ttk_textfont)*((ttk_screen->w < 160 || ttk_screen->w >= 320)?2:3), ret->w-20, 10+ttk_text_height(ttk_textfont), 0, "/mnt/", save_folder);
+	ttk_add_widget(ret, wid);
+	wid2 = ttk_new_widget(10, 5);
+	wid2->w = ret->w-20;
+	wid2->h = ttk_text_height(ttk_menufont)*((ttk_screen->w < 160 || ttk_screen->w >= 320)?2:3);
+	wid2->draw = select_folder_draw;
+	ttk_add_widget(ret, wid2);
+	ret = pz_finish_window(ret);
+	ti_widget_start_softdep(wid);
+	return ret;
+        }
+        pz_warning("Tiwidgets module is missing");
+        return TTK_MENU_DONOTHING;
+}
+
+static void cleanup()
 {
 	pz_browser_remove_handler(is_gba);
 }
 
-static void init_igpsp() 
+static void init() 
 {
-        struct stat gbaBuf;
+        struct stat st;
 
-	igpsp_module = pz_register_module("igpSP", igpsp_cleanup);
-	igpsp_binary = pz_module_get_datapath(igpsp_module, "igpSP");
-	igpsp_conf = pz_load_config(pz_module_get_cfgpath(igpsp_module,"igpsp_dir.conf"));
-
-        if (!stat(igpsp_binary, &gbaBuf) == S_IXUSR || 00100)
-            chmod(igpsp_binary, S_IRWXU);
+	module = pz_register_module("igpSP", cleanup);
+	binary = pz_module_get_datapath(module, "igpSP");
+	config = pz_load_config(pz_module_get_cfgpath(module,"igpsp_dir.conf"));
+             
+        if (!stat(binary, &st) == S_IXUSR || 00100)
+            chmod(binary, S_IRWXU);
 	
-	igpsp_browser_extension.name = N_("Open with GBA");
-	igpsp_browser_extension.makesub = load_igpsp_handler;
-	pz_browser_add_action (is_gba, &igpsp_browser_extension);
-	pz_browser_set_handler(is_gba, igpsp_load_file);
+	browser_extension.name = N_("Open with Game Boy Advance");
+	browser_extension.makesub = load_handler;
+	pz_browser_add_action (is_gba, &browser_extension);
+	pz_browser_set_handler(is_gba, exec_file);
 
-        pz_menu_add_action_group("/Extras/Games/Game Boy Advance", "Browse", browse_igpsp_roms);
-        pz_menu_add_action_group("/Setting/Set directory/Set Game Boy Advance", "Browse", igpsp_directory);
+        pz_menu_add_action_group("/Extras/Games/Game Boy Advance", "Browse", browse_roms);
+        pz_menu_add_action_group("/Setting/Select folder/Game Boy Advance", "Browse", select_folder);
 }
 
-PZ_MOD_INIT(init_igpsp)
+PZ_MOD_INIT(init)
